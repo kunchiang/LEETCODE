129a30d9a10c49eb27a9415b9e07578aed997bf1
chiang <ichiangkun@gmail.com>
2019-04-09 17:43:22 +0800

    WA

--- 32.最长有效括号.py
+++ 32.最长有效括号.py
@@ -29,6 +29,53 @@
 # 
 #
 class Solution:
-    def longestValidParentheses(self, s: str) -> int:
-        
+
+    def longestValidParentheses(self, s):
+        n = len(s)
+        if n <= 1:
+            return 0
+        dp = [0]*(n)
+        stack = []
+        gap = 0
+        count = 0
+        sp = True
+        if s[0] == '(':
+            stack.append('(')
+        for i in range(1, n):
+            if s[i] == '(':
+                stack.append(s[i])
+                dp[i] = dp[i-1]
+                if dp[i] > 0 and not sp:
+                    gap += 1
+                    count = 0
+                sp = False
+            else:
+                if len(stack) == 0:
+                    dp[i] = dp[i-1]
+                    sp = True
+                    gap = 0
+                else:
+                    if gap == 0:
+                        dp[i] = dp[i-1] + 1
+                    else:
+                        count += 1
+                        gap -= 1
+                        if gap == 0:
+                            dp[i] = dp[i-1]+count
+                        else:
+                            dp[i] = max(dp[i-1], count)
+                    stack.pop()
+        print(dp)
+        return 2*dp[n-1]
+
+sol = Solution()
+# print(sol.longestValidParentheses("()"))
+# assert sol.longestValidParentheses("()") == 2
+# assert sol.longestValidParentheses(")()())") == 4
+# assert sol.longestValidParentheses("((()(())())") == 10
+# assert sol.longestValidParentheses(")))))(((()(") == 2
+assert sol.longestValidParentheses("()))(())") == 4
+assert sol.longestValidParentheses("()((()()") == 4
+# assert sol.longestValidParentheses("())())()())") == 4
+
 
